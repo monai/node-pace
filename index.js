fb12fc24dd64d8c095ff1a2706a1b932409d5b3c
monai <monai@cure.lt>
2020-09-02 09:46:38 +0300

    Cleanup

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
 let addon;
 
 try {
-  console.log('debug');
   addon = require('./build/Debug/addon.node');
 } catch (ex) {
-  console.log('release');
   addon = require('./build/Release/addon.node');
 }
 

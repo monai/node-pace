27539b622b76db8ed2518a8a9a512c419ce2d3d5
monai <monai@cure.lt>
2020-09-10 12:28:01 +0300

    Add keyAgreement function

--- src/addon.cpp
+++ src/addon.cpp
@@ -1,6 +1,7 @@
 #include "napi.h"
 #include "pace/ecdh_gm_map_napi.hpp"
 #include "pace/ecdh_gm_generate_keys_napi.hpp"
+#include "pace/ecdh_gm_key_agreement_napi.hpp"
 
 #include <node_api.h>
 
@@ -24,6 +25,14 @@ napi_value Init(napi_env env, napi_value exports) {
                      &gm_generate_keys_fn));
   NAPI_CALL(env, napi_set_named_property(env, gm, "generateKeys",
                                          gm_generate_keys_fn));
+
+  napi_value gm_key_agreement_fn;
+  NAPI_CALL(env, napi_create_function(
+                     env, "keyAgreement", NAPI_AUTO_LENGTH,
+                     pace::ecdh_gm_key_agreement::napi::key_agreement, nullptr,
+                     &gm_key_agreement_fn));
+  NAPI_CALL(env, napi_set_named_property(env, gm, "keyAgreement",
+                                         gm_key_agreement_fn));
 
   NAPI_CALL(env, napi_set_named_property(env, exports, "gm", gm));
 
